1011919db29a63e608d75f856e542f75e5562047
MirnavSteenbergen <47982411+MirnavSteenbergen@users.noreply.github.com>
2020-04-17 18:46:12 +0200

    Project creation + implementation Menu
    Created Unity Project
    Implemented UI and Menu scenes

---
+++ Project_Gecko/Assets/scripts/Levelselect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Levelselect : MonoBehaviour
+{
+    public int SelectedLevel;
+
+    public void LoadLevelSelected()
+    {
+        SceneManager.LoadScene(SelectedLevel);
+    }
+}
---
+++ Project_Gecko/Assets/scripts/MenuScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ToLevelSelect()
+    {
+        SceneManager.LoadScene("LevelSelect");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
