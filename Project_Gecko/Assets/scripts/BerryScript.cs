c38741dcd242c182e4ee0ceb85fab9bbc60d9cad
MirnavSteenbergen <47982411+MirnavSteenbergen@users.noreply.github.com>
2020-04-21 14:15:00 +0200

    Berry Pick-up
    implemented berry pick-up
    created mirna testscene

---
+++ Project_Gecko/Assets/scripts/BerryScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BerryScript : MonoBehaviour
+{
+    BoxCollider2D col;
+    SpriteRenderer theSprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<BoxCollider2D>();
+        theSprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+    public void BerryPickUp(float powerupDuration)
+    {
+        col.enabled = false;
+        theSprite.enabled = false;
+        StartCoroutine(RespawnBerry(powerupDuration));
+    }
+
+    IEnumerator RespawnBerry(float powerupDuration)
+    {
+        yield return new WaitForSeconds(powerupDuration);
+        col.enabled = true;
+        theSprite.enabled = true;
+    }
+}
---
+++ Project_Gecko/Assets/scripts/PlayerScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float StartMovespeed = 1;
+    public float Movespeed = 1;
+    public float MovespeedBoostMultiplier = 2;
+    public float powerupDuration = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Berry"))
+        {
+            other.gameObject.GetComponent<BerryScript>().BerryPickUp(powerupDuration);
+            Movespeed *= MovespeedBoostMultiplier;
+            StartCoroutine( MsDecayRoutine());
+        }
+    }
+
+    IEnumerator MsDecayRoutine()
+    {
+        float step = 1f / powerupDuration;
+        for (float i = 0f; i < 1f; i += step * Time.deltaTime)
+        {
+            //Player. = Mathf.Lerp(Movespeed, StartMovespeed, i);
+            yield return null;
+        }
+    }
+}
