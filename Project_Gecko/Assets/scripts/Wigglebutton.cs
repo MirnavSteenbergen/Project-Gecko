a937e4d8377caaa601ca8b85c4c1d29d2b7f57b1
MirnavSteenbergen <47982411+MirnavSteenbergen@users.noreply.github.com>
2020-05-06 17:44:23 +0200

    switched platforms to android
    switched platforms to android and added ui for mobile controls

---
+++ Project_Gecko/Assets/scripts/Wigglebutton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wigglebutton : MonoBehaviour
+{
+    public GameObject gecko;
+
+    public void buttonpressed()
+    {
+        gecko.GetComponent<wigglescript>().Wiggle();
+    }
+
+}
--- Project_Gecko/Assets/scripts/wigglescript.cs
+++ Project_Gecko/Assets/scripts/wigglescript.cs
@@ -14,29 +14,24 @@ public class wigglescript : MonoBehaviour
         theSprite = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-       if (Input.GetKeyDown(KeyCode.Z))
-        {
-            if (wiggleCount == fall)
-            {
-                GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-            }
 
-            if (theSprite.flipY == false)
-            {
-                theSprite.flipY = true;
-                wiggleCount += 1;
-            }
-            else
-            {
-                theSprite.flipY = false;
-                wiggleCount += 1;
-            }
+    public void Wiggle()
+    {
+        if (wiggleCount == fall)
+        {
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
         }
 
-       if (wiggleCount == fall) { }
-            
+        if (theSprite.flipY == false)
+        {
+            theSprite.flipY = true;
+            wiggleCount += 1;
+        }
+        else
+        {
+            theSprite.flipY = false;
+            wiggleCount += 1;
+        }
+
     }
 }
