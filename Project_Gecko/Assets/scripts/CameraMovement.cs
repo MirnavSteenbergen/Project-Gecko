d0340cf4a3fd3eb774471753940a8e3578119e84
Joeri Ridder <joeriridder@LIVE.NL>
2020-05-31 15:10:16 +0200

    Fixed aspect ration in build (I think)

--- Project_Gecko/Assets/scripts/CameraMovement.cs
+++ Project_Gecko/Assets/scripts/CameraMovement.cs
@@ -7,20 +7,28 @@ public class CameraMovement : MonoBehaviour
     private Camera cam;
     private BoxCollider2D roomColl;
     //public Vector2 aspectRatio = new Vector2(16, 9);
-    public Vector2 roomSize;
+    //public Vector2 roomSize;
 
     private void Awake()
     {
         cam = GetComponent<Camera>();
         roomColl = GetComponent<BoxCollider2D>();
 
-        cam.orthographicSize = roomSize.y * 0.5f;
-        roomColl.size = new Vector2(roomSize.x, roomSize.y);
+        //cam.orthographicSize = roomSize.y * 0.5f;
+        //roomColl.size = new Vector2(roomSize.x, roomSize.y);
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        if (Screen.width < Screen.height)
+        {
+            Screen.SetResolution(Screen.height / 7 * 12, Screen.height, FullScreenMode.MaximizedWindow);
+        }
+        else
+        {
+            Screen.SetResolution(Screen.width, Screen.width / 12 * 7, FullScreenMode.MaximizedWindow);
+        }
         
     }
 
@@ -42,7 +50,7 @@ public class CameraMovement : MonoBehaviour
             if (collider.transform.position.y > roomColl.bounds.max.y) dirY += 1;
             if (collider.transform.position.y < roomColl.bounds.min.y) dirY -= 1;
 
-            transform.position += new Vector3(dirX * roomSize.x, dirY * roomSize.y);
+            transform.position += new Vector3(dirX * collider.bounds.size.x, dirY * collider.bounds.size.y);
         }
     }
 }
