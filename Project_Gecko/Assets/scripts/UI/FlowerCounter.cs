c252147f11da8cfca6509349f710c023a2c8815b
MirnavSteenbergen <47982411+MirnavSteenbergen@users.noreply.github.com>
2020-06-11 14:38:13 +0200

    flowers pick-up and ui counter
    also carries over from level to level
    made a new canvas prefab with this included

---
+++ Project_Gecko/Assets/scripts/UI/FlowerCounter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlowerCounter : MonoBehaviour
+{
+    public static int flowerCount;
+    public Text Text;
+
+    void Start()
+    {
+        flowerCount = PlayerPrefs.GetInt("Flowers");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Text.text = "" + flowerCount;
+    }
+
+    public static void addFlower ()
+    {
+        flowerCount += 1;
+        PlayerPrefs.SetInt("Flowers", flowerCount);
+    }
+
+}
